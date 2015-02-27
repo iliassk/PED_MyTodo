c7c44f322620eacf2ac2deaa44e65e42ff049b19
iliass <kenfaoui.iliass@gmail.com>
2015-02-27 15:37:37 +0100

    [END] #TestValinscr Tests E2E de register fonctionnels

--- frontend/test/e2e/register-spec.js
+++ frontend/test/e2e/register-spec.js
@@ -32,4 +32,44 @@ describe('ToDoManager e2e register tests', function() {
 
 	}, 10000);
 
+
+	it('should not allow registration if Username exists', function() {
+		browser.get('#/register');
+
+		expect(element(by.id('btnRegister')).isEnabled()).toBe(false);
+		browser.findElement(protractor.By.id('username')).sendKeys('tester2');
+		browser.findElement(protractor.By.id('email')).sendKeys('test2_bis@test.com');
+		browser.findElement(protractor.By.id('password')).sendKeys('test2');
+		browser.findElement(protractor.By.id('password_confirmation')).sendKeys('test2');
+		expect(element(by.id('btnRegister')).isEnabled()).toBe(true);
+
+		browser.findElement(protractor.By.id('btnRegister')).click();
+		browser.waitForAngular();
+
+
+		expect(element(by.id('logout')).isDisplayed()).toBeFalsy();
+
+		expect(browser.getCurrentUrl()).toBe(browser.baseUrl + '#/register');
+
+	}, 10000);
+
+	it('should not allow registration if Email exists', function() {
+		browser.get('#/register');
+
+		expect(element(by.id('btnRegister')).isEnabled()).toBe(false);
+		browser.findElement(protractor.By.id('username')).sendKeys('tester2_bis');
+		browser.findElement(protractor.By.id('email')).sendKeys('test2@test.com');
+		browser.findElement(protractor.By.id('password')).sendKeys('test2');
+		browser.findElement(protractor.By.id('password_confirmation')).sendKeys('test2');
+		expect(element(by.id('btnRegister')).isEnabled()).toBe(true);
+
+		browser.findElement(protractor.By.id('btnRegister')).click();
+		browser.waitForAngular();
+
+
+		expect(element(by.id('logout')).isDisplayed()).toBeFalsy();
+
+		expect(browser.getCurrentUrl()).toBe(browser.baseUrl + '#/register');
+
+	}, 10000);
 });
