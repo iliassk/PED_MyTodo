44667a256239c1bc4a198e0d109b738a4a9ce990
aminux <slaoui.andaloussi.amine@gmail.com>
2015-03-04 15:09:26 +0100

    réglage de problème d'affichage contact && todolist

--- frontend/app/javascript/left_menu.js
+++ frontend/app/javascript/left_menu.js
@@ -4,19 +4,27 @@ function toggle(anId)
 	
 	if (node.id=="listTodo")
 	{
+
+		$("#groupe").children().hide();
+		$("#listTodo").children().show();
 		// Contenu caché, le montrer
 		node.style.visibility = "visible";
 		node.style.height = "auto";
+
 		document.getElementById("groupe").style.visibility = "hidden";
+		document.getElementById("groupe").style.height = "0";
 		document.getElementById("addgroup").style.visibility = "hidden";
 		document.getElementById("addgroup").style.height = "0";
+	
 					
 	}
 	if(node.id=="groupe")
 	{
-
+		$("#listTodo").children().hide();
+		$("#groupe").children().show();
 		node.style.visibility = "visible";
 		node.style.height = "auto";
+
 		document.getElementById("listTodo").style.visibility = "hidden"
 		document.getElementById("listTodo").style.height = "0"
 		// Contenu visible, le cacher
