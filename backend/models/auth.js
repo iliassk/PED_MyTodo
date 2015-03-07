1b1a77599bcd5d14a4b14e5b1ab8df77dd3416aa
iliass <kenfaoui.iliass@gmail.com>
2015-03-07 20:35:08 +0100

    authFacebook fonctionnel avec nouveau fichier config.js

--- backend/models/auth.js
+++ backend/models/auth.js
@@ -1,8 +1,10 @@
 /*var jwt = require('jwt-simple');*/
 var moment = require('moment');
 var bcrypt = require('bcrypt-nodejs');
+var qs = require('querystring');
 
 var auth = require('./auth.js');
+var config = require('../services/config.js');
 
 
 exports.checkAuthorization = function(req, res, jwt) {
@@ -26,8 +28,6 @@ exports.checkAuthorization = function(req, res, jwt) {
 
 exports.createSendToken = function(data, connection, req, res, jwt) {
 
-	var expires = moment().add(2, 'days').valueOf();
-
 	connection.query('SELECT * FROM USERS WHERE email = ?', data.email, function(err, rows) {
 		if (err) {
 			console.log(err);
@@ -39,7 +39,7 @@ exports.createSendToken = function(data, connection, req, res, jwt) {
 		var payload = {
 			iss: req.hostname,
 			sub: rows[0].id_user,
-			exp: expires
+			exp: moment().add(2, 'days').unix()
 		}
 
 		var token = jwt.encode(payload, "AGKYW");
@@ -60,14 +60,14 @@ exports.comparePasswords = function(password, passwordDb, callback) {
 }
 
 
-exports.loginGoogle = function(req, res, next, connection, jwt, request) {
+exports.authGoogle = function(req, res, next, connection, jwt, request) {
 	var url = 'https://accounts.google.com/o/oauth2/token';
 	var apiUrl = 'https://www.googleapis.com/plus/v1/people/me/openIdConnect';
 
 	var params = {
 		code: req.body.code,
 		client_id: req.body.clientId,
-		client_secret: 'IMVbg7p3HeO_zwqxkGH_s4fh',
+		client_secret: config.GOOGLE_SECRET,
 		redirect_uri: req.body.redirectUri,
 		grant_type: 'authorization_code'
 	}
@@ -126,5 +126,70 @@ exports.loginGoogle = function(req, res, next, connection, jwt, request) {
 				});
 			});
 		})
+	});
+}
+
+exports.authFacebook = function(req, res, next, connection, jwt, request) {
+	var accessTokenUrl = 'https://graph.facebook.com/oauth/access_token';
+	var graphApiUrl = 'https://graph.facebook.com/me';
+
+	var params = {
+		code: req.body.code,
+		client_id: req.body.clientId,
+		client_secret: config.FACEBOOK_SECRET,
+		redirect_uri: req.body.redirectUri
+	};
+
+	// get call from node to facebook
+	request.get({
+		url: accessTokenUrl,
+		qs: params //accessToken comes in a querystring format
+	}, function(err, response, accessToken) {
+		accessToken = qs.parse(accessToken);
+
+		// Get the Facebook profile informations
+		request.get({
+			url: graphApiUrl,
+			qs: accessToken,
+			json: true
+		}, function(err, response, profile) {
+			connection.query('SELECT * FROM USERS WHERE facebookId = ?', profile.id, function(err, rows) {
+				if (err)
+					return next(err);
+
+				if (rows.length == 1) {
+					return auth.createSendToken(rows[0], connection, req, res, jwt);
+				}
+
+				var data = {
+					username: profile.name,
+					email: profile.email,
+					password: profile.id, // To add an extra security
+					facebookId: profile.id
+				};
+
+				//Hash passwords
+				bcrypt.genSalt(10, function(err, salt) {
+					if (err) return next(err);
+
+					bcrypt.hash(data.password, salt, null, function(err, hash) {
+						// Store hash in your password DB.
+						if (err) return next(err);
+
+						data.password = hash;
+
+						//insertion 
+						connection.query('INSERT INTO USERS SET ?', data, function(err, rows) {
+							if (err) {
+								console.log(err);
+								return next("Mysql error on register, check your query");
+							}
+
+							auth.createSendToken(data, connection, req, res, jwt)
+						});
+					});
+				});
+			});
+		});
 	});
 }
