87b909394c39eeb5cfaf6b3ebcd5689e0befbfda
vwilmet <33bubulle33@gmail.com>
2015-03-09 18:41:52 +0100

    Gestion du popover html!!!

--- frontend/app/scripts/controllers/viewToDoList.js
+++ frontend/app/scripts/controllers/viewToDoList.js
@@ -13,6 +13,16 @@ angular.module('ToDoManagerApp').controller('ViewToDoList', function($scope, $st
 	$scope.list_id = $stateParams.id
 	$scope.todos
 
+	$scope.popup = {
+  		content: '<p>Popup content here</p>',
+  		options: {
+  			html: true,
+    		title: null,
+    		placement: 'bottom'
+  		}
+	}; 
+
+
 	$scope.fetchData = function(){
 		TDMService.fetchToDoListToDos($scope.list_id)
 		.success(function(data) {
@@ -41,33 +51,28 @@ angular.module('ToDoManagerApp').controller('ViewToDoList', function($scope, $st
 
 	}
 
-	$scope.addToCalendar = function(todo){
-		console.log(todo);
-
-		
-		//google
-		/*
+	$scope.addToCalendar = function(type, todo){
 		var date = "20140510/20150514";
-
-		var url = 
-		  "https://www.google.com/calendar/render?action=TEMPLATE" + 
-		  "&text=" + encodeURIComponent(todo.title) +
-		  "&dates=" + encodeURIComponent(date) +
-		  "&details=" + encodeURIComponent(todo.description) +
-		  "&location="+ encodeURIComponent(todo.localization)+
-		  "&pli=1"+
-		  "&uid=&sf=false&output=xml#g";
-		*/
-
-		//hotmail
-		var url = 
-			"https://bay03.calendar.live.com/calendar/calendar.aspx?rru=addevent" +
-			"&dtstart=20140510"+
-			"&dtend=20150514"+
-			"&summary=" + encodeURIComponent(todo.title) +
-			"&location="+ encodeURIComponent(todo.localization)+
-			"&description=" + encodeURIComponent(todo.description) +
-			"&uid="
+		var url = "";
+		if(type === 'google'){
+			url = 
+				"https://www.google.com/calendar/render?action=TEMPLATE" + 
+				"&text=" + encodeURIComponent(todo.title) +
+				"&dates=" + encodeURIComponent(date) +
+				"&details=" + encodeURIComponent(todo.description) +
+				"&location="+ encodeURIComponent(todo.localization)+
+				"&pli=1"+
+				"&uid=&sf=false&output=xml#g";
+		}else if(type === 'hotmail'){
+			url = 
+				"https://bay03.calendar.live.com/calendar/calendar.aspx?rru=addevent" +
+				"&dtstart=20140510"+
+				"&dtend=20150514"+
+				"&summary=" + encodeURIComponent(todo.title) +
+				"&location="+ encodeURIComponent(todo.localization)+
+				"&description=" + encodeURIComponent(todo.description) +
+				"&uid="
+		}
 
 		$window.open(url, '_blank');
 	}
@@ -89,4 +94,35 @@ angular.module('ToDoManagerApp').controller('ViewToDoList', function($scope, $st
 
 	$scope.fetchData();
 
+});
+
+angular.module('ToDoManagerApp').
+directive('popup', function() {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    scope: {
+      ngModel: '=',
+      options: '=popup'
+    },
+    link: function(scope, element) {
+      scope.$watch('ngModel', function(val) {
+        element.attr('data-content', val);
+      });
+
+      var options = scope.options || {} ; 
+
+      var title = options.title || null;
+      var placement = options.placement || 'right';
+      var html = options.html || false;
+      var delay = options.delay ? angular.toJson(options.delay) : null;
+      var trigger = options.trigger || 'hover';
+
+      element.attr('title', title);
+      element.attr('data-placement', placement);
+      element.attr('data-html', html);
+      element.attr('data-delay', delay);
+      element.popover({ trigger: trigger });
+    }
+  };
 });
