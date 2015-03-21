4f876ab522a4ccf08e19c52f8d23227f8ebf02d8
vwilmet <33bubulle33@gmail.com>
2015-03-21 16:48:28 +0100

    Passage en online .... au cas ou pour ceux qui ne serait pas faire le changement

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -23,7 +23,7 @@ angular.module('ToDoManagerApp', ['ui.router','ui.calendar', 'ngAnimate','ui.boo
   	$rootScope.isWorking = false
     $rootScope.canFetchData = false
 
-    $rootScope.online = false
+    $rootScope.online = true
 
     if($rootScope.online == false){
       TDMService.markAsOffLine()
